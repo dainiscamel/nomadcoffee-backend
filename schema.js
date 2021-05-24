b5ec688d9e11c931751e81019dd94c57e8b91d64
kangdain <juhyueon3@naver.com>
2021-05-25 01:23:04 +0900

    Set up a prisma project

---
+++ schema.js
@@ -0,0 +1,17 @@
+import {
+  loadFilesSync,
+  makeExecutableSchema,
+  mergeResolvers,
+  mergeTypeDefs,
+} from "graphql-tools";
+
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const loadedResolvers = loadFilesSync(
+  `${__dirname}/**/*.{queries,resolvers}.js`
+);
+
+export const typeDefs = mergeTypeDefs(loadedTypes);
+export const resolvers = mergeResolvers(loadedResolvers);
+
+// const schema = makeExecutableSchema({ typeDefs, resolvers });
+// export default schema;
